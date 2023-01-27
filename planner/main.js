2eee9e13a49227f90cdd81d5de1bc0cb99bfbded
luke <luke09@live.fr>
2023-01-27 16:17:17 +0100

    fix specialisation worker

--- planner/main.js
+++ planner/main.js
@@ -10,7 +10,7 @@ const taskType = () => (randInt(0, 2) ? 'mult' : 'add')
 const args = () => ({ a: randInt(0, 40), b: randInt(0, 40) })
 
 const generateTasks = (i) =>
-  new Array(i).fill(1).map((_) => ({ type: taskType(), args: args() }))
+new Array(i).fill(1).map((_) => ({ type: taskType(), args: args() }))
 
 
 let workers = [
@@ -27,13 +27,21 @@ app.use(
 
 app.get('/', (req, res) => {
   res.send(JSON.stringify(workers))
+  console.log(JSON.stringify(workers))
 })
 
 app.post('/register', (req, res) => {
-  const { url, id } = req.body
-  console.log(`Register: adding ${url} worker: ${id}`)
-  workers.push({ url, id })
+  let { url, id, worktype } = req.body;
+  console.log(`Register: adding ${url} workerID: ${id}, worktype: `,worktype)
+  let findWorker = workers.find(w => w.id===id)
+
+  if(!findWorker){
+    workers.push({ url, id, worktype })
+  }
+
   res.send('ok')
+
+  console.log(JSON.stringify(workers))
 })
 
 let tasks = generateTasks(nbTasks)
@@ -43,10 +51,31 @@ const wait = (mili) =>
   new Promise((resolve, reject) => setTimeout(resolve, mili))
 
 const sendTask = async (worker, task) => {
-  console.log(`=> ${worker.url}/${task.type}`, task)
+   // console.log(`=> ${worker.url}/${task.type}`, task)
+
   workers = workers.filter((w) => w.id !== worker.id)
-  tasks = tasks.filter((t) => t !== task)
-  const request = fetch(`${worker.url}:8080/${task.type}`, {
+
+  tasks = tasks.filter((t) => t !== task);
+
+  //console.log('task before sending request', tasks)
+
+
+  if(!worker.worktype){
+    //generalist
+    console.log(`${worker.url}/${task.type}`);
+  }
+  else{
+    if(task.type != worker.worktype){
+      console.log('wrong task continue...')
+      return;
+    }
+  }
+
+
+
+
+
+  const request = fetch(`${worker.url}/${task.type}`, {
     method: 'POST',
     headers: {
       Accept: 'application/json',
@@ -55,15 +84,16 @@ const sendTask = async (worker, task) => {
     body: JSON.stringify(task.args),
   })
     .then((res) => {
+
       workers = [...workers, worker]
       return res.json()
     })
     .then((res) => {
       taskToDo -= 1
-      console.log('---')
+      /*console.log('---')
       console.log(nbTasks - taskToDo, '/', nbTasks, ':')
       console.log(task, 'has res', res)
-      console.log('---')
+      console.log('---')*/
       return res
     })
     .catch((err) => {
@@ -73,11 +103,11 @@ const sendTask = async (worker, task) => {
 }
 
 const main = async () => {
-  console.log(tasks)
   while (taskToDo > 0) {
     await wait(100)
     if (workers.length === 0 || tasks.length === 0) continue
     sendTask(workers[0], tasks[0])
+
   }
   console.log('end of tasks')
   server.close()
